9117fb5f871dd272db86f3579ba41b4bfae960cd
Claudio <claudiotf@gmail.com>
2025-03-21 14:57:59 -0300

    ajustes no layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:vertex/simple_page.dart';
-import 'settings_page.dart';
-import 'lista_page.dart';
-import 'contador_page.dart';
-import 'database_test.dart';
-import 'tasks_page.dart';
+import 'package:vertex/settings_page.dart';
+import 'package:vertex/lista_page.dart';
+import 'package:vertex/contador_page.dart';
+import 'package:vertex/database_test.dart';
+import 'package:vertex/tasks_page.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:vertex/main_layout.dart';
 
@@ -21,40 +21,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomePage(),
+      home: MainLayout(
+        title: 'Dashboard',
+        child: const HomeContent(),
+        isHomePage: true, // Define como página inicial
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+// Conteúdo da HomePage separado como um widget independente
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Dashboard', // Define o título
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min, // Mantém a altura apenas necessária
-          mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
-          children: [
-            Text(
-              'Bem Vindo!',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // Mantém a altura apenas necessária
+        mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
+        children: [
+          Text(
+            'Bem Vindo!',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
             ),
-            SizedBox(height: 20), // Adiciona um espaço entre o texto e a imagem
-            Image.asset(
-              'assets/logo.png',
-              width: 100,
-              height: 100,
-            ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20), // Adiciona um espaço entre o texto e a imagem
+          Image.asset(
+            'assets/logo.png',
+            width: 100,
+            height: 100,
+          ),
+        ],
       ),
-
     );
   }
 }
--- lib/main_layout.dart
+++ lib/main_layout.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:vertex/simple_page.dart';
-import 'settings_page.dart';
-import 'lista_page.dart';
-import 'contador_page.dart';
-import 'database_test.dart';
-import 'tasks_page.dart';
-import 'main.dart';
-
-import 'dart:typed_data';
-import 'package:flutter/services.dart';
+import 'package:vertex/settings_page.dart';
+import 'package:vertex/lista_page.dart';
+import 'package:vertex/contador_page.dart';
+import 'package:vertex/database_test.dart';
+import 'package:vertex/tasks_page.dart';
+import 'package:vertex/main.dart'; // Importa HomeContent
 
 class MainLayout extends StatefulWidget {
   final String title;
   final Widget child;
+  final bool isHomePage; // Nova propriedade para identificar a HomePage
   final Widget? floatingActionButton;
 
   const MainLayout({
     super.key,
     required this.title,
     required this.child,
+    this.isHomePage = false, // Padrão é false
     this.floatingActionButton,
   });
 
@@ -27,56 +27,135 @@ class MainLayout extends StatefulWidget {
 }
 
 class _MainLayoutState extends State<MainLayout> {
+  DateTime? _lastPressed;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
-          style: const TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blueAccent,
-        foregroundColor: Colors.white,
-      ),
-      floatingActionButton: widget.floatingActionButton,
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(color: Colors.blueAccent),
-              child: Align(
-                alignment: Alignment.centerLeft, // Alinha todo o conteúdo à esquerda
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // Mantém os elementos alinhados à esquerda
-                  mainAxisSize: MainAxisSize.min, // Impede que a coluna ocupe todo o espaço vertical
-                  children: [
-                    const Text(
-                      'Menu',
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    const SizedBox(height: 8), // Espaço entre o texto e a imagem
-                    SizedBox(
-                      width: 90,
-                      height: 90,
-                      child: Image.asset('assets/logo-inverse.png', fit: BoxFit.contain),
-                    ),
-                  ],
-                ),
+    return WillPopScope(
+      onWillPop: () async {
+        debugPrint('Is HomePage: ${widget.isHomePage}, child type: ${widget.child.runtimeType}');
+
+        if (!widget.isHomePage) {
+          debugPrint('Navigating to HomePage');
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MainLayout(
+                title: 'Dashboard',
+                child: const HomeContent(),
+                isHomePage: true,
               ),
             ),
+                (Route<dynamic> route) => false,
+          );
+          return false; // Não fecha o app
+        }
 
-            _buildMenuItem(context, Icons.dashboard, 'Dashboard', const HomePage()),
-            _buildMenuItem(context, Icons.pages, 'Página Simples', const SimplePage()),
-            _buildMenuItem(context, Icons.list, 'Lista', const ListaPage()),
-            _buildMenuItem(context, Icons.plus_one, 'Contador', const ContadorPage()),
-            _buildMenuItem(context, Icons.settings, 'Configurações', SettingsPage()),
-            _buildMenuItem(context, Icons.task, 'Tarefas', TasksPage()),
-            _buildMenuItem(context, Icons.checklist_rtl, 'DB Teste', DatabaseTestPage()),
-          ],
+        final now = DateTime.now();
+        const exitDelay = Duration(seconds: 2);
+
+        if (_lastPressed == null || now.difference(_lastPressed!) > exitDelay) {
+          _lastPressed = now;
+          debugPrint('Showing SnackBar');
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Pressione novamente para sair"),
+              duration: exitDelay,
+            ),
+          );
+          return false; // Não fecha ainda
+        }
+
+        debugPrint('Exiting app');
+        return true; // Fecha o app
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
         ),
+        floatingActionButton: widget.floatingActionButton,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(color: Colors.blueAccent),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Menu',
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                      const SizedBox(height: 8),
+                      SizedBox(
+                        width: 90,
+                        height: 90,
+                        child: Image.asset('assets/logo-inverse.png', fit: BoxFit.contain),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.dashboard,
+                'Dashboard',
+                MainLayout(
+                  title: 'Dashboard',
+                  child: const HomeContent(),
+                  isHomePage: true,
+                ),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.pages,
+                'Página Simples',
+                MainLayout(title: 'Página Simples', child: const SimplePage()),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.list,
+                'Lista',
+                MainLayout(title: 'Lista', child: const ListaPage()),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.plus_one,
+                'Contador',
+                MainLayout(title: 'Contador', child: const ContadorPage()),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.settings,
+                'Configurações',
+                MainLayout(title: 'Configurações', child: SettingsPage()),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.task,
+                'Tarefas',
+                MainLayout(title: 'Tarefas', child: TasksPage()),
+              ),
+              _buildMenuItem(
+                context,
+                Icons.checklist_rtl,
+                'DB Teste',
+                MainLayout(title: 'DB Teste', child: DatabaseTestPage()),
+              ),
+            ],
+          ),
+        ),
+        body: widget.child,
       ),
-      body: widget.child,
     );
   }
 
@@ -85,7 +164,7 @@ class _MainLayoutState extends State<MainLayout> {
       leading: Icon(icon),
       title: Text(text),
       onTap: () {
-        Navigator.pop(context);
+        Navigator.pop(context); // Fecha o drawer
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => page),
@@ -94,4 +173,3 @@ class _MainLayoutState extends State<MainLayout> {
     );
   }
 }
-
